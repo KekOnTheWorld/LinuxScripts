b380b125e051b527fe0de51801938c4baecf89aa
KekOnTheWorld <kekontheworld@gmail.com>
2022-02-05 13:29:51 +0100

    Updates

---
+++ README.md
@@ -0,0 +1,17 @@
+# Linux Scripts
+
+A bunch of little linux tools made by KekOnTheWorld!
+
+<br>
+
+## Requirements
+1. Python
+2. NPM and NodeJS
+
+<br>
+
+## Installation
+```
+chmod +x install.sh
+./install.sh
+```
--- install.sh
+++ install.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/bash
 
+
 python3 -m pip install requests
-sudo chmod +x kekflix
+chmod +x kekflix
+sudo npm install -g peerflix
 sudo cp kekflix /usr/bin
+
+printf "\n\033[38;2;100;255;100mSuccessfully installed linux scripts!\033[0m\n"
